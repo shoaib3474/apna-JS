2ab83c9cd44faa3f8e4b002321db97715bf8640f
shoaib3474 <shoaib3974@gmail.com>
2025-04-27 16:13:32 +0500

    string and its properties

---
+++ Loops & String/String/script.js
@@ -0,0 +1,16 @@
+const str = "Pakistan"
+//length 
+console.log(str.length)
+console.log(str.toUpperCase())
+//remove spaces around 
+ const fullName = "          Header ali           "
+console.log(fullName.trim())
+const sport = "cricket"
+console.log(sport.slice(1 ,4))
+
+console.log(sport.replaceAll("cricket", "football"))
+
+
+ const newstr = fullName.concat(sport)
+
+ console.log(newstr)
